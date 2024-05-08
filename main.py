4303083fb6127e1e9ab357d3f12e740f9b7dd826
Jaroslav <novak.jn.111@gmail.com>
2024-05-08 17:35:22 +0200

    correction of .py files import

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ df_data = data_transform.main()
 ############################## NASTAVENI PARAMETRU A SPUSTENI FUNKCI PRO TOVRBU GRAFU ########################################
 
 # TODO Monthly evaluation table
-df_pnl_monthly = pnl_monthly(df_data)
+df_pnl_monthly = data_transform.pnl_monthly(df_data)
 
 # TODO Monthly evaluation table plot
 #monthly_sums(df, column)
@@ -25,11 +25,11 @@ cols_portfolio = ['pnl_balancing_portfolio', 'odchylka_mnozstvi', 'system_imbala
 units = ['[CZK]', '[MWh]', '[MWh]','[CZK/MWH]','[CZK/MWH]']
 dt_from = '2023-01-01 00:00:00+01:00'
 dt_to = '2023-01-31 00:00:00+01:00'
-hourly_ts_plots(df_data, cols_portfolio, units, dt_from, dt_to)
+data_transform.hourly_ts_plots(df_data, cols_portfolio, units, dt_from, dt_to)
 
 cols_b2c = ['pnl_balancing_b2c', 'deviation_b2c', 'system_imbalance', 'imbalance', 'counter_imbalance']
 units = ['[CZK]', '[MWh]', '[MWh]','[CZK/MWH]','[CZK/MWH]']
 dt_from = '2023-01-01 00:00:00+01:00'
 dt_to = '2023-01-31 00:00:00+01:00'
 
-hourly_ts_plots(df_data, cols_b2c, units, dt_from, dt_to)
+data_transform.hourly_ts_plots(df_data, cols_b2c, units, dt_from, dt_to)
